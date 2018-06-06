a55bd0fa1fffd06b5c1073dae6adc4c6a1d5877a
yojiwatanabe <yojiwata@gmail.com>
2018-06-06 12:03:07 -0400

    added comments, cleaned up formatting

--- dumpPluginOut.py
+++ dumpPluginOut.py
@@ -4,7 +4,8 @@ from securitycenter import SecurityCenter5
 import json
 import getpass
 
-HOST 	  = 'sec-center-prod-01.uit.tufts.edu'
+HOST = 'sec-center-prod-01.uit.tufts.edu'
+
 
 # 		loginSC()
 #
@@ -14,29 +15,33 @@ HOST 	  = 'sec-center-prod-01.uit.tufts.edu'
 # Input  - none
 # Output - SecurityCenter5 object of an authenticated connection
 def loginSC():
-	user 	= raw_input("Username: ")
-	pw 		= getpass.getpass()
-	sc 		= SecurityCenter5(HOST)
+    user = raw_input("Username: ")
+    pw   = getpass.getpass()
+    sc   = SecurityCenter5(HOST)
 
-	sc.login(user, pw)
-	return sc
+    sc.login(user, pw)
+    return sc
 
-sc 			= loginSC()
-output 		= sc.analysis(('pluginID', '=', '22869'), tool='vulndetails')
-case_num 	= 1
-total_cases = len(output)
-obj 		= []
-temp_obj 	= {'ID':'', 'IP':'', 'DNS':'', 'REPO':'', 'CONTENT':[]}
+
+# Establish connection, retrieve data
+sc = loginSC()
+output = sc.analysis(('pluginID', '=', '22869'), tool='vulndetails')
+
+# Build JSON structure with data retrieved
+case_num = 1
+obj = []
+temp_obj = {'ID': '', 'IP': '', 'DNS': '', 'REPO': '', 'CONTENT': []}
 for case in output:
-	temp_obj['ID'] 		= case_num
-	temp_obj['IP'] 		= case[u'ip']
-	temp_obj['DNS'] 	= case[u'dnsName']
-	temp_obj['REPO'] 	= case[u'repository'][u'name']
-	program_list 		= case[u'pluginText'].split("\n");
-	temp_obj['CONTENT'] = program_list[3:-1]
-	obj.append(temp_obj.copy())
-	case_num += 1
+    program_list        = case[u'pluginText'].split("\n")
+    temp_obj['ID']      = case_num
+    temp_obj['IP']      = case[u'ip']
+    temp_obj['DNS']     = case[u'dnsName']
+    temp_obj['REPO']    = case[u'repository'][u'name']
+    temp_obj['CONTENT'] = program_list[3:-1]
+    obj.append(temp_obj.copy())
+    case_num += 1
 
+# Convert to JSON, write to file
 ob = json.dumps(obj)
 f = open('pluginText.dump', 'w')
 f.write(ob)
