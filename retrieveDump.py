7838eaed5b52a4c9177488a8ac735c9e36aca8b9
yojiwatanabe <yojiwata@gmail.com>
2018-06-06 12:04:55 -0400

    added comments, refactored code into functions

--- retrieveDump.py
+++ retrieveDump.py
@@ -4,9 +4,26 @@ import json
 
 DUMP_FILE = 'pluginText.dump'
 
-f = open(DUMP_FILE, 'r')
-rawData = json.load(f)
-f.close()
 
-for value in rawData:
-    print value['DNS']
+# 		loadData():()
+#
+# Function to load JSON information from a file stream
+# Input  - none
+# Output - Python dictionary with data
+def loadData():
+    f = open(DUMP_FILE, 'r')
+    rawData = json.load(f)
+    f.close()
+
+    return rawData
+
+
+def main():
+    data = loadData()
+
+    return 0
+
+
+# # # # # # # # # # # # # # # # # # # # # # #
+if __name__ == '__main__':
+    main()
