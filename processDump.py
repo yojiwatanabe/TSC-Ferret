1cfbcdbea09d187cf6bced44dd50a635d53dd94b
yojiwatanabe <yojiwata@gmail.com>
2018-06-06 16:48:56 -0400

    cleanup

--- processDump.py
+++ processDump.py
@@ -6,38 +6,43 @@ import numpy as np
 DUMP_FILE = 'pluginText.dump'
 INPUT_FILE = 'programs.txt'
 
+
 # 		loadData():()
 #
 # Function to load JSON information from a file stream
 # Input  - none
 # Output - Python dictionary with data
 def loadData():
-    f = open(DUMP_FILE, 'r')
+    f       = open(DUMP_FILE, 'r')
     rawData = json.load(f)
     f.close()
     return rawData
 
+
 def readInput():
-	f = open(INPUT_FILE, 'r')
-	input = f.read().splitlines()
-	return input
+    f    = open(INPUT_FILE, 'r')
+    data = f.read().splitlines()
+    return data
+
 
 def createMatrix(data, inputData):
-    resultMat = np.empty((len(data), len(inputData)), dtype = object)
+    resultMat = np.empty((len(data), len(inputData)), dtype=object)
     for i, host in enumerate(data):
         for j, inputProgram in enumerate(inputData):
             tempList = []
             for program in host['CONTENT']:
                 if inputProgram in program:
                     tempList.append(program)
+
             resultMat[i][j] = tempList
     return resultMat
+
 
 def main():
     data = loadData()
     inputData = readInput()
     resultMat = createMatrix(data, inputData)
-    
+
     print resultMat
     return 0
 
