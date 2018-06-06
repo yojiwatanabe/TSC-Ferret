45c7db721c348d6149e8220cf41d1b576725ed62
yojiwatanabe <yojiwata@gmail.com>
2018-06-06 10:16:13 -0400

    added id column, cleaned up login code

--- dumpPluginOut.py
+++ dumpPluginOut.py
@@ -1,33 +1,43 @@
+# -*- coding: utf-8 -*-
+
 from securitycenter import SecurityCenter5
 import json
 import getpass
 
-HOST = 'sec-center-prod-01.uit.tufts.edu'
+HOST 	  = 'sec-center-prod-01.uit.tufts.edu'
 
-user = input("Username: ")
-pw = getpass.getpass()
-sc = SecurityCenter5(HOST)
-sc.login(user, pw)
+# 		loginSC()
+#
+# Function to open a connection with the specified Security Center 5 host. Asks
+# user for their login information and then proceeds to try to establish an 
+# authenticated connection.
+# Input  - none
+# Output - SecurityCenter5 object of an authenticated connection
+def loginSC():
+	user 	= raw_input("Username: ")
+	pw 		= getpass.getpass()
+	sc 		= SecurityCenter5(HOST)
 
+	sc.login(user, pw)
+	return sc
 
-output = sc.analysis(('pluginID', '=', '22869'), tool='vulndetails')
-case_num = 1
+sc 			= loginSC()
+output 		= sc.analysis(('pluginID', '=', '22869'), tool='vulndetails')
+case_num 	= 1
 total_cases = len(output)
-obj = []
-temp_obj = {'IP':'', 'DNS':'', 'REPO':'', 'CONTENT':''}
+obj 		= []
+temp_obj 	= {'ID':'', 'IP':'', 'DNS':'', 'REPO':'', 'CONTENT':''}
 for case in output:
-	# print '##### CASE ' + str(case_num) + "/" + str(total_cases)
-	temp_obj['IP'] = case[u'ip']
-	# print 'IP Address: ' + case[u'ip']
-	temp_obj['DNS'] = case[u'dnsName']
-	# print 'DNS: ' + case[u'dnsName']
-	temp_obj['REPO'] = case[u'repository'][u'name']
+	temp_obj['ID'] 		= case_num
+	temp_obj['IP'] 		= case[u'ip']
+	temp_obj['DNS'] 	= case[u'dnsName']
+	temp_obj['REPO'] 	= case[u'repository'][u'name']
 	temp_obj['CONTENT'] = case[u'pluginText']
-	# print 'Repository: ' + case[u'repository'][u'name']
-	# print case[u'pluginText']
+
 	obj.append(json.dumps(temp_obj))
 	temp_obj.clear()
-ob = json.dumps(obj)
+	case_num += 1
 
+ob = json.dumps(obj)
 f = open('pluginText.dump', 'w')
 f.write(ob)
