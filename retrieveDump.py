16339e482040cffbc3956807c24137be5da034bf
yojiwatanabe <yojiwata@gmail.com>
2018-06-06 11:53:40 -0400

    updated readin method given changes to dumpPlugin.py

--- retrieveDump.py
+++ retrieveDump.py
@@ -8,8 +8,5 @@ f = open(DUMP_FILE, 'r')
 rawData = json.load(f)
 f.close()
 
-formattedDict = []
-for i in range(len(rawData)):
-    formattedDict.append(json.loads(rawData[i]))
-
-del f, rawData
+for value in rawData:
+    print value['DNS']
