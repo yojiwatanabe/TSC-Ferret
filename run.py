51e36a89086dfea111088f4000e9e1be52134b4e
gyawalisaurav <saurav.gyawali@yahoo.com>
2018-06-07 15:59:22 -0400

    Added headers

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# This is the main driver file that the user runs. It takes command line
+# arguments and calls all of the other modules. There is one positional
+# argument that this file takes and one optional should follow -i
+
 import sys
 import argparse
 import dumpPluginOut
@@ -12,7 +16,7 @@ parser.add_argument('pluginID', help = 'Plugin ID for the desired plugin output'
 parser.add_argument('-i', dest= 'iFile', help = 'Input file')
 args = parser.parse_args()
 
-#dumpPluginOut.dumpPluginData(args.pluginID)
+dumpPluginOut.dumpPluginData(args.pluginID)
 
 if args.iFile:
     processDump.createTable(args.pluginID, args.iFile)
