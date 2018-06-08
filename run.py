d34bd34be68f2c86a942682f92c256c6523eb00d
yojiwatanabe <yojiwata@gmail.com>
2018-06-08 14:11:57 -0400

    began adding host/repo querying

--- run.py
+++ run.py
@@ -11,17 +11,30 @@ import processDump
 from sys import exit
 
 
-parser = argparse.ArgumentParser(description = 'Helper script to retrieve \
-                                 plugin output from Service Center scans')
-parser.add_argument('pluginID', help = 'Plugin ID for the desired plugin output')
-parser.add_argument('-i', dest= 'iFile', help = 'Input file')
-args = parser.parse_args()
-try:
-	dumpPluginOut.dumpPluginData(args.pluginID)
-	processDump.createTable(args.pluginID, args.iFile)
-except Exception as e:
-	print 'ERROR CODE [' + str(e.code) + ']:'
-	print e.msg.encode('ascii')
-	exit(e.code)
+def initiateArgParse():
+    parser = argparse.ArgumentParser(description='Helper script to retrieve plugin output from Service Center scans')
+    parser.add_argument('pluginID', help='Plugin ID for the desired plugin output')
+    parser.add_argument('-i', dest='search_list', help='Input file for words to query output')
+    parser.add_argument('-R', dest='repo_list', help='Input file for repositories to query')
+    parser.add_argument('-H', dest='host_list', help='Input file for hosts to query')
 
-print "done"
+    return parser.parse_args()
+
+
+def main():
+    args = initiateArgParse()
+
+    try:
+        dumpPluginOut.dumpPluginData(args.pluginID)
+        processDump.createTable(args.pluginID, args.iFile)
+    except Exception as e:
+        print 'ERROR CODE [' + str(e.code) + ']:'
+        print e.msg.encode('ascii')
+        exit(e.code)
+
+    print "done"
+
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+if __name__ == '__main__':
+    main()
