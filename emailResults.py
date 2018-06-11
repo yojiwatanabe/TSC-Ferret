0283e91648bfc0d9f2d7c9d30cc0027cb9380c38
yojiwatanabe <yojiwata@gmail.com>
2018-06-11 12:23:24 -0400

    first stages of emailing results

--- emailResults.py
+++ emailResults.py
@@ -3,4 +3,32 @@
 
 '''
 emailResults.py
+
+This module works to email the results of the plugin output query. It is able to read the recipients list from a file
+as well as read in from the command line. It uses an SMTP server to send out each message attached with the results
+table created from the last query.
 '''
+
+#       getRecipients()
+#
+#
+def getRecipients():
+    # try to find a file with email addresses
+    # if file doesnt exist or is empty,
+        # read in addresses manually
+
+    # give user prompt about exactly what users are being sent the data
+    # have user confirm, allow them to go back and edit
+
+
+def messageSetup():
+    # function to create the message that will be sent
+
+    # get sender
+    # call on function to get recipients
+    # create descriptive subject line
+    # MAKE HEADER
+
+    # attach results.html
+
+    # try send / except errorMsg
