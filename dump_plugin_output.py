ba6ca967094bcd43c023f11bc627e39684bfb3c7
yojiwatanabe <yojiwata@gmail.com>
2018-06-18 14:22:49 -0400

    refactored get_repo_ids() function for legibility

--- dump_plugin_output.py
+++ dump_plugin_output.py
@@ -13,7 +13,6 @@ module and made human-friendly.
 
 import json
 import getpass
-# import ipaddress
 import process_dump
 from securitycenter import SecurityCenter5
 
@@ -37,116 +36,20 @@ def login_sc():
     return sc
 
 
-# 		dump_data_repo_query()
+#       get_repo_ids()
 #
-# Function to save all queried information for hosts in a specified repository/repositories
-# Input  - repo_list: string list with repositories to include in output
-#          output: dictionary list with the plugin output from all hosts/repos with host information
-# Output - dictionary list with the plugin output of hosts in the specified repository
-# def dump_data_repo_query(repo_list, output):
-#     case_num = 1
-#     obj = []
-#     temp_obj = {'ID': '', 'IP': '', 'DNS': '', 'REPO': '', 'CONTENT': []}
-#     for case in output:
-#         if case[u'repository'][u'name'] not in repo_list:
-#             continue
-#         temp_obj['ID'] = case_num
-#         temp_obj['IP'] = case[u'ip']
-#         temp_obj['MAC'] = case[u'macAddress']
-#         temp_obj['DNS'] = case[u'dnsName']
-#         temp_obj['REPO'] = case[u'repository'][u'name']
-#         temp_obj['L_SEEN'] = case[u'lastSeen']
-#         temp_obj['CONTENT'] = case[u'pluginText'].split("\n")
+# Takes in the requested repository names and all repository data and returns the requested repositories' IDs, which is
+# used by dump_plugin_data() to request scan information about specific repositories
+def get_repo_ids(requested_repo_names, all_repo_data):
+    repo_ids = []
 
-#         obj.append(temp_obj.copy())
-#         case_num += 1
-
-#     return obj
-
-
-# 		dump_data_host_query()
-#
-# Function to save all queried information for the specified host/hosts
-# Input  - host_list: string list with IP addresses of hosts to include in output
-#          output: dictionary list with the plugin output from all hosts/repos with host information
-# Output - dictionary list with the plugin output of hosts within the defined IP address range
-# def dump_data_host_query(host_list, output):
-#     case_num = 1
-#     obj = []
-#     temp_obj = {'ID': '', 'IP': '', 'DNS': '', 'REPO': '', 'CONTENT': []}
-#     for case in output:
-#         if case[u'ip'] not in host_list:
-#             continue
-#         temp_obj['ID'] = case_num
-#         temp_obj['IP'] = case[u'ip']
-#         temp_obj['MAC'] = case[u'macAddress']
-#         temp_obj['DNS'] = case[u'dnsName']
-#         temp_obj['REPO'] = case[u'repository'][u'name']
-#         temp_obj['L_SEEN'] = case[u'lastSeen']
-#         temp_obj['CONTENT'] = case[u'pluginText'].split('\n')
-
-#         obj.append(temp_obj.copy())
-#         case_num += 1
-
-#     return obj
-
-
-#       ip_in_range()
-#
-# Function to check if the given ip address is within a given range
-# Input  - ip_min : String, with IP lower bound
-#        - ip_max : String, with IP upper bound
-#        - ip : String, with IP address to check
-# Output -
-# def ip_in_range(ip_min, ip_max, ip):
-#     ip_min = ipaddress.IPv4Address(ip_min.decode('utf-8'))
-#     ip_max = ipaddress.IPv4Address(ip_max.decode('utf-8'))
-#     ip = ipaddress.IPv4Address(ip.decode('utf-8'))
-
-#     if ip_min <= ip <= ip_max:
-#         return True
-
-#     return False
-
-
-# 		dump_data_ip_range()
-#
-# Function to save all queried information that falls within the range of IPs
-# Input  - ip_min: String, lower IP address boundary
-#          ip_max: String, upper IP address boundary
-#          output: dictionary list with the plugin output from all hosts/repos with host information
-# Output - dictionary list with the plugin output of hosts within the defined IP address range
-# def dump_data_ip_range(ip_min, ip_max, output):
-#     case_num = 1
-#     obj = []
-#     temp_obj = {'ID': '', 'IP': '', 'DNS': '', 'REPO': '', 'CONTENT': []}
-#     for case in output:
-#         if ip_in_range(ip_min, ip_max, case[u'ip']):
-#             temp_obj['ID'] = case_num
-#             temp_obj['IP'] = case[u'ip']
-#             temp_obj['MAC'] = case[u'macAddress']
-#             temp_obj['DNS'] = case[u'dnsName']
-#             temp_obj['REPO'] = case[u'repository'][u'name']
-#             temp_obj['L_SEEN'] = case[u'lastSeen']
-#             temp_obj['CONTENT'] = case[u'pluginText'].split('\n')
-
-#             obj.append(temp_obj.copy())
-#             case_num += 1
-
-#     return obj
-
-def repoID_gen(repo_list, repo_json):
-    repo_IDs = []
-
-    for repo_name in repo_list:
-        for repos in repo_json['response']:
-            if repo_name == repos['name']:
-                repo_IDs.append(repos['id'])
+    for requested_repo_name in requested_repo_names:
+        for repository in all_repo_data['response']:
+            if requested_repo_name == repository['name']:
+                repo_ids.append(repository['id'])
                 break
 
-    return ",".join(repo_IDs)
-
-
+    return ",".join(repo_ids)
 
 
 # 		dump_plugin_data()
@@ -155,16 +58,16 @@ def repoID_gen(repo_list, repo_json):
 # about the desired plugin, and dumps it all to a .dump file.
 # Input  - plugin_id, a string of the plugin_id whose output is to be dumped
 # Output - none, write to file
-def dump_plugin_data(plugin_id, repo_list, host_list, ip_range):
+def dump_plugin_data(plugin_id, requested_repo_names, host_list, ip_range):
     # Establish connection, retrieve data
     sc = login_sc()
     arg_tuples = [('pluginID', '=', plugin_id)]
 
-    if repo_list:
-        repo_list = process_dump.read_input(repo_list)
-        repo_data = sc.get('/repository')
-        repo_comma_list = repoID_gen(repo_list, repo_data.json())
-        arg_tuples.append(('repositoryIDs', '=', repo_comma_list))
+    if requested_repo_names:
+        requested_repo_names = process_dump.read_input(requested_repo_names)
+        all_repo_data = sc.get('/repository')
+        requested_repo_ids = get_repo_ids(requested_repo_names, all_repo_data.json())
+        arg_tuples.append(('repositoryIDs', '=', requested_repo_ids))
 
     if host_list:
         hosts = process_dump.read_input(host_list)
@@ -173,7 +76,7 @@ def dump_plugin_data(plugin_id, repo_list, host_list, ip_range):
     elif ip_range:
         arg_tuples.append(('ip', '=', ip_range))
 
-    output = sc.analysis(*arg_tuples, tool = 'vulndetails')
+    output = sc.analysis(*arg_tuples, tool='vulndetails')
 
     case_num = 1
     obj = []
