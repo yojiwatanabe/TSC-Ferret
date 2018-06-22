5cfa7c60de3db212e2c9750aa774e2f511783eb7
yojiwatanabe <yojiwata@gmail.com>
2018-06-22 14:35:55 -0400

    added support for pdf output

--- run.py
+++ run.py
@@ -5,12 +5,12 @@
 # arguments and calls all of the other modules. There is one positional
 # argument that this file takes and one optional should follow -i
 
+import json
 import argparse
-import dump_plugin_output
 import process_dump
 import email_results
+import dump_plugin_output
 from sys import exit
-import json
 
 
 #       initiate_argparse()
@@ -20,9 +20,12 @@ import json
 # Output - argparse Argument Parser object
 def initiate_argparse():
     parser = argparse.ArgumentParser(description='Helper script to retrieve plugin output from Service Center scans')
-    group = parser.add_mutually_exclusive_group(required=True)
+    plugin_source = parser.add_mutually_exclusive_group(required=True)
+    output_type = parser.add_mutually_exclusive_group(required=False)
 
-    group.add_argument('-p', '--plugin_id', dest='plugin_id', help='Plugin ID for the desired plugin output')
+    plugin_source.add_argument('-P', '--plugin_id', dest='plugin_id', help='Plugin ID for the desired plugin output')
+    plugin_source.add_argument('-C', '--config', dest='config', help='Config file to load credentials and arguments')
+
     parser.add_argument('-s', '--search_queries', dest='search_list', help='Input file for words to query output '
                                                                            '(e.g. -s queries.txt)')
     parser.add_argument('-R', '--repo_list', dest='repos', help='Input file for repositories to query '
@@ -31,13 +34,15 @@ def initiate_argparse():
                                                                 '(e.g. -H hosts.txt)')
     parser.add_argument('-i', '--ip_range', dest='ip_range', help='Range of IPs from which to gather data '
                                                                   '(e.g. --ip_range 127.0.0.1-192.168.0.1)')
-    parser.add_argument('-c', '--csv_out', dest='csv', help='Change from default html output to a CSV output',
-                        default=False, action='store_true')
-    group.add_argument('-C', '--config', dest='config', help='Config file to load credentials and arguments')
     parser.add_argument('-d', '--allow_duplicates', dest='duplicates', help='Change from default behavior of only '
                         'outputting latest scan results to show all results', default=False, action='store_true')
     parser.add_argument('-e', '--email_results', dest='email_results', help='Email results of TSC Search to the given '
                         'recipients', default=False, action='store_true')
+
+    output_type.add_argument('-c', '--csv_out', dest='csv', help='Change from default html output to a CSV output',
+                             default=False, action='store_true')
+    output_type.add_argument('-p', '--pdf_out', dest='pdf', help='Change from default html output to a PDF output',
+                             default=False, action='store_true')
 
     return parser.parse_args()
 
@@ -45,30 +50,45 @@ def initiate_argparse():
 def main():
     args = initiate_argparse()
     is_csv = False
+    is_pdf = False
     try:
         if args.config:
             f = open(args.config, 'r')
             config = json.loads(f.read())
+
             is_csv = config['csv']
-            dump_plugin_output.dump_plugin_data(config['plugin_id'], config['repo_list'], config['host_list'], config['ip_range'],
-                                                config['duplicates'], config['user'], config['pass'])
-            process_dump.create_table(is_csv, config['search_list'])
+            is_pdf = config['pdf']
+
+            dump_plugin_output.dump_plugin_data(config['plugin_id'], config['repo_list'], config['host_list'],
+                                                config['ip_range'], config['duplicates'], config['user'],
+                                                config['pass'])
+            process_dump.create_table(is_csv, is_pdf, config['search_list'])
         else:
             is_csv = args.csv
-            dump_plugin_output.dump_plugin_data(args.plugin_id, args.repos, args.hosts, args.ip_range, args.duplicates, '', '')
-            process_dump.create_table(is_csv, args.search_list)
+            is_pdf = args.pdf
+
+            dump_plugin_output.dump_plugin_data(args.plugin_id, args.repos, args.hosts, args.ip_range, args.duplicates,
+                                                '', '')
+            process_dump.create_table(is_csv, is_pdf, args.search_list)
+
+        if args.email_results:
+            email_results.craft_and_send_message(args.plugin_id, args.hosts, args.repos, args.ip_range,
+                                                 args.search_list, args.duplicates, is_csv, is_pdf)
 
     except Exception as e:
         print '\n###### ERROR'
         print 'Exception: [' + str(e) + ']:'
         exit(1)
-    except KeyboardInterrupt as k:
+
+    except KeyboardInterrupt:
         print '\n###### Keyboard Interrupt'
         print 'exiting...'
         exit(1)        
 
     if is_csv:
         print "Created " + process_dump.CSV_OUTPUT
+    elif is_pdf:
+        print "Created " + process_dump.PDF_OUTPUT
     else:
         print "Created " + process_dump.HTML_OUTPUT
 
